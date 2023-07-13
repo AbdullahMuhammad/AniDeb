58436291c4eee63e4596f8377587ee2a37973457
abdullahmuhammad <abdullahmuhammadbasra@gmail.com>
2023-07-13 20:03:18 +0500

    Populate statuses table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_13_122653) do
+ActiveRecord::Schema.define(version: 2023_07_13_145925) do
 
   create_table "active_admin_comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "namespace"
